2f45225c693b9c7f00cd964c45d7c923bba897f7
gpBlockchain <744158715@qq.com>
2023-09-19 16:37:11 +0800

    update comment link

--- ckb-bench-server/run.sh
+++ ckb-bench-server/run.sh
@@ -5,4 +5,4 @@ bash script/benchmark.sh clean
 python script/gen_report.py
 report=`cat demo.md`
 export GITHUB_TOKEN=${GITHUB_TOKEN}
-bash script/ok.sh add_comment gpBlockchain/test-action 3 "$report"
+bash script/ok.sh add_comment nervosnetwork/ckb-integration-test 16 "$report"
