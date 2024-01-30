c9a2a926ed97c2d6939a597e0b81cfd92333e6af
gpBlockchain <32102187+gpBlockchain@users.noreply.github.com>
2024-01-30 23:35:34 +0800

    Update v2021.rs

--- ckb-bench/src/rpc/v2021.rs
+++ ckb-bench/src/rpc/v2021.rs
@@ -9,5 +9,7 @@ jsonrpc!(pub struct Inner2021 {
 
     pub fn send_transaction(&self, tx: Transaction, outputs_validator: Option<String>) -> H256;
     pub fn get_transaction(&self, _hash: H256) -> Option<TransactionWithStatusResponse>;
+    pub fn send_test_transaction(&self, tx: Transaction, outputs_validator: Option<String>) -> H256;
+
 
 });
