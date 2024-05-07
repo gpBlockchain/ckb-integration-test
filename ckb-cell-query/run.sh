5df354461d1f47a704763a0bcf914f0401b8376e
gpBlockchain <744158715@qq.com>
2024-05-07 22:13:28 +0800

    add script

--- ckb-cell-query/run.sh
+++ ckb-cell-query/run.sh
@@ -1,2 +1,6 @@
 bash script/run.sh setup
 bash script/run.sh run 98400f6a67af07025f5959af35ed653d649f745b8f54bf3f07bef9bd605ee946.1w.1024w.cell
+python wkr.py
+report=`cat demo.md`
+export GITHUB_TOKEN=${GITHUB_TOKEN}
+bash script/ok.sh add_comment nervosnetwork/ckb 2372 "$report"
