6c650fa26e5cb83cfb1037e89ee1fbe4dedaf346
gpBlockchain <744158715@qq.com>
2024-05-09 23:52:38 +0800

    fix bench

--- ckb-cell-query/run.sh
+++ ckb-cell-query/run.sh
@@ -1,6 +1,6 @@
 bash script/run.sh setup
 bash script/run.sh run 98400f6a67af07025f5959af35ed653d649f745b8f54bf3f07bef9bd605ee946.1w.1024w.cell
-bash script/run.sh run bench
+bash script/run.sh bench
 
 pip install qiniu
 python script/gen_report.py
